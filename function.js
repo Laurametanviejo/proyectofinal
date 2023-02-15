f0096bde486ee04004a06ff82907557b4e2f3c6d
Laurametanviejo <laurametanviejo666@gmail.com>
2023-02-15 11:50:14 -0300

    proyectofinal

---
+++ function.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick = function () {
+    console.log ("Mi mayor espectativa es formar parte de esta destacada empresa.")
+    document.getElementById("sobremi").innerHTML = "Soy una profesional capaz de comprender y manejar las formas de comunicación del siglo XXI."
+    
+}
+
+
+
